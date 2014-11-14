7192bb7cdc142c31ed2799057bc9fa2ec10f7507
msi <msierbin@gmail.com>
2014-11-14 11:17:53 +0100

    Solved Beta 2010 task in PHP - comment

--- Beta2010.php
+++ Beta2010.php
@@ -1,4 +1,29 @@
-function solution($A) {
+/* Given an array A of N integers, we draw N discs in a 2D plane such that 
+ * the I-th disc is  *centered on (0,I) and has a radius of A[I]. We say 
+ * that the J-th disc and K-th disc intersect if  *J ? K and J-th and K-th 
+ * discs have at least one common point.
+ * Write a function: int solution(int A[]);
+ * that, given an array A describing N discs as explained above, returns 
+ * the number of pairs of intersecting discs. For example, given N=6 and:
+ * A[0] = 1  A[1] = 5  A[2] = 2 
+ * A[3] = 1  A[4] = 4  A[5] = 0  
+ * intersecting discs appear in eleven pairs of elements:
+ * 0 and 1,
+ * 0 and 2,
+ * 0 and 4,
+ * 1 and 2,
+ * 1 and 3,
+ * 1 and 4,
+ * 1 and 5,
+ * 2 and 3,
+ * 2 and 4,
+ * 3 and 4,
+ * 4 and 5.
+ * so the function should return 11.
+ * The function should return -1 if the number of intersecting pairs 
+ * exceeds 10,000,000.function 
+ */
+solution($A) {
   $result = 0;
   $len = count($A);
     
